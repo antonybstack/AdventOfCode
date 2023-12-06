473fc76b72fde3de2aa730a12cad3858d933fbe0
antonybstack <antonyblyakher@gmail.com>
2023-12-05 19:17:39 -0500

    stub out remaining day solution templates

---
+++ AdventOfCode/Day20/Day20.cs
@@ -0,0 +1,30 @@
+namespace AdventOfCode.Day20;
+
+public sealed class Day20 : PuzzleSolver
+{
+    public override dynamic SolvePart1(byte[] input)
+    {
+        return Part1.Run(input);
+    }
+
+    public override dynamic SolvePart2(byte[] input)
+    {
+        return Part2.Run(input);
+    }
+
+    private static class Part1
+    {
+        public static int Run(Span<byte> input)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    private static class Part2
+    {
+        public static int Run(Span<byte> input)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ AdventOfCode/Day29/Day29.cs
@@ -0,0 +1,30 @@
+namespace AdventOfCode.Day29;
+
+public sealed class Day29 : PuzzleSolver
+{
+    public override dynamic SolvePart1(byte[] input)
+    {
+        return Part1.Run(input);
+    }
+
+    public override dynamic SolvePart2(byte[] input)
+    {
+        return Part2.Run(input);
+    }
+
+    private static class Part1
+    {
+        public static int Run(Span<byte> input)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    private static class Part2
+    {
+        public static int Run(Span<byte> input)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
